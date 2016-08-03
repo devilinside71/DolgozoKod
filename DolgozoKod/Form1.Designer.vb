ffb0d4aa07bf2e27052e1a63d49eb1f538ff187a
Laszlo Tamas <laszlo.devil@gmail.com>
2016-08-03 09:24:20 +0200

    Start

---
+++ DolgozoKod/Form1.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.ComboBoxPrinter = New System.Windows.Forms.ComboBox()
+        Me.ButtonPrint = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxName = New System.Windows.Forms.TextBox()
+        Me.TextBoxKod = New System.Windows.Forms.TextBox()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.TableLayoutPanel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 79.78142!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.21858!))
+        Me.TableLayoutPanel1.Controls.Add(Me.ComboBoxPrinter, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.ButtonPrint, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel2, 0, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 76.24521!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 23.75479!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(549, 261)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'ComboBoxPrinter
+        '
+        Me.ComboBoxPrinter.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ComboBoxPrinter.FormattingEnabled = True
+        Me.ComboBoxPrinter.Location = New System.Drawing.Point(3, 201)
+        Me.ComboBoxPrinter.Name = "ComboBoxPrinter"
+        Me.ComboBoxPrinter.Size = New System.Drawing.Size(431, 21)
+        Me.ComboBoxPrinter.TabIndex = 0
+        '
+        'ButtonPrint
+        '
+        Me.ButtonPrint.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ButtonPrint.Location = New System.Drawing.Point(440, 201)
+        Me.ButtonPrint.Name = "ButtonPrint"
+        Me.ButtonPrint.Size = New System.Drawing.Size(106, 57)
+        Me.ButtonPrint.TabIndex = 1
+        Me.ButtonPrint.Text = "Nyomtat"
+        Me.ButtonPrint.UseVisualStyleBackColor = True
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.ColumnCount = 2
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.2181!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.78191!))
+        Me.TableLayoutPanel2.Controls.Add(Me.Label1, 0, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.Label2, 0, 1)
+        Me.TableLayoutPanel2.Controls.Add(Me.TextBoxName, 1, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.TextBoxKod, 1, 1)
+        Me.TableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(3, 3)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 2
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(431, 192)
+        Me.TableLayoutPanel2.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Label1.Location = New System.Drawing.Point(33, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 96)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Dolgozó neve:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Label2.Location = New System.Drawing.Point(37, 96)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 96)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Dolgozói kód:"
+        '
+        'TextBoxName
+        '
+        Me.TextBoxName.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBoxName.Location = New System.Drawing.Point(115, 3)
+        Me.TextBoxName.Name = "TextBoxName"
+        Me.TextBoxName.Size = New System.Drawing.Size(313, 20)
+        Me.TextBoxName.TabIndex = 2
+        '
+        'TextBoxKod
+        '
+        Me.TextBoxKod.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBoxKod.Location = New System.Drawing.Point(115, 99)
+        Me.TextBoxKod.Name = "TextBoxKod"
+        Me.TextBoxKod.Size = New System.Drawing.Size(313, 20)
+        Me.TextBoxKod.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(549, 261)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "Form1"
+        Me.Text = "Dolgozói kód nyomtató"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel2.ResumeLayout(False)
+        Me.TableLayoutPanel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents ComboBoxPrinter As ComboBox
+    Friend WithEvents ButtonPrint As Button
+    Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBoxName As TextBox
+    Friend WithEvents TextBoxKod As TextBox
+End Class
---
+++ DolgozoKod/Form1.vb
@@ -0,0 +1,36 @@
+﻿
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Call ZebraPrint.LoadPrinters()
+        ComboBoxPrinter.Items.Clear()
+        For Each pr In ZebraPrint.PrinterNames
+            ComboBoxPrinter.Items.Add(pr)
+        Next
+        ComboBoxPrinter.SelectedIndex = 0
+        Call ZebraPrint.LoadLabels()
+    End Sub
+    Sub ZPLPrint()
+        Dim strPrinter As String
+        Dim strPrintText As String
+        Dim res As String
+
+        Dim strVonalkod As String
+        Dim strDolgozoNev As String
+
+
+        strPrinter = ZebraPrint.PrinterWinNames(ComboBoxPrinter.SelectedIndex)
+        strVonalkod = TextBoxKod.Text
+        strDolgozoNev = TextBoxName.Text
+        strPrintText = ZebraPrint.LabelCodes(0)
+        strPrintText = strPrintText.Replace("VONALKOD", strVonalkod)
+        strPrintText = strPrintText.Replace("DOLGOZONEV", strDolgozoNev)
+        strPrintText = strPrintText.Replace("DOLGOZOUTF", ZebraPrint.GetZPLutf8Code(strDolgozoNev))
+
+        res = ZebraPrint.SendStringToPrinter(strPrinter, strPrintText)
+
+    End Sub
+
+    Private Sub ButtonPrint_Click(sender As Object, e As EventArgs) Handles ButtonPrint.Click
+        Call ZPLPrint()
+    End Sub
+End Class
